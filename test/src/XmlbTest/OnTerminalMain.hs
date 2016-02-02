cc06b566adeb7ea552c9409ec5d8ec7084e6e06a
Maksim Golubev <maksim.golubev@billing.ru>
2016-02-02 23:36:12 +0300

    Revert "fix: tests tolerance if not on terminal device"
    This reverts commit 20a66361881126283c4e74a9ac7ce308b61f3e13.

--- test/src/XmlbTest/OnTerminalMain.hs
+++ test/src/XmlbTest/OnTerminalMain.hs
@@ -2,7 +2,12 @@ module Main where
 
 import XmlbSpec (specOnlyOnTerminal)
 import Test.Hspec (hspec)
+import System.IO (stdout, hIsTerminalDevice)
 
 main :: IO ()
-main = hspec specOnlyOnTerminal
+main = do
+  stdout_isatty <- hIsTerminalDevice stdout
+  if stdout_isatty
+    then hspec specOnlyOnTerminal
+    else error "STDOUT must be a terminal device for running this test suite"
        
